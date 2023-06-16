88770099b3a35fc72ed831238bddd60d019bc681
Jasper-Rowan <jasper.rowan@berkeley.edu>
2023-06-16 16:14:57 -0700

    :rocket: LETS GOOO got it working

--- sst.config.ts
+++ sst.config.ts
@@ -16,22 +16,29 @@ export default {
     app.stack(function Stack({ stack }) {
       const opencvLayerArn = "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-python38-opencv-python-headless:11";
       const libgthreadLayerArn = "arn:aws:lambda:us-east-1:770693421928:layer:Klayers-python38-libgthread-so:1";
+      
+      const layer1 = lambda.LayerVersion.fromLayerVersionArn(stack, "OpencvLayer", opencvLayerArn);
+      const layer2 = lambda.LayerVersion.fromLayerVersionArn(stack, "LibgthreadLayer", libgthreadLayerArn);
 
       const api = new Api(stack, "api", {
         routes: {
-          "GET /": "functions/lambda.handler",
-        },
-        functionProps: {
-          layers: [
-            lambda.LayerVersion.fromLayerVersionArn(stack, "OpencvLayer", opencvLayerArn),
-            lambda.LayerVersion.fromLayerVersionArn(stack, "LibgthreadLayer", libgthreadLayerArn),
-          ],
+          "GET /": {
+            function: {
+              handler: "functions/lambda.handler",
+              // The chrome-aws-lambda layer currently does not work in Node.js 16
+              runtime: "python3.8",
+              // Increase the timeout for generating screenshots
+              timeout: 15,
+              // Load Layer
+              layers: [layer1, layer2],
+            },
+          },
         },
       });
 
       stack.addOutputs({
         ApiEndpoint: api.url,
-        Funcname: api.getFunction.name,
+        FunctionName: api.getFunction("GET /")?.functionName,
       });
     });
   },
